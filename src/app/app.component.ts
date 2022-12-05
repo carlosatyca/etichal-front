fbe8d22939635fd69b28182e48650cedbda5c176
Carlos Atienza <72754340+carlosatyca@users.noreply.github.com>
2022-12-05 22:13:06 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  constructor(private ApiService: ApiService) { }
+  callAPI(){
+    console.log("Button clicked")
+    this.ApiService.getWifi();
+  }
 }
